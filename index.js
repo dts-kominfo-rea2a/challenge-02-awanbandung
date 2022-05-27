d9c1c0186775e2530e7a3478db46ef988feca1af
Wawan Rudiana N <awanbandung@gmail.com>
2022-05-27 21:08:05 +0700

    add console.log komentar

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
